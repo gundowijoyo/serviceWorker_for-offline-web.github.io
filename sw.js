37a0898c519891629616d4c84920843bcf6dec93
wijoyoCell <133162689+wijoyoCell@users.noreply.github.com>
2023-05-22 19:33:38 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,15 +2,20 @@ self.addEventListener('install',function(event){
 event.waitUntil(
 caches.open('caku-v5').then(function(cache){
 return cache.addAll([
-'/index.html',
-  
+'/'
+ '/index.html', 
 ])
 })
 
 
 )
 })
-self.addEventListener('fetch', function(request){
-opendWith()
+self.addEventListener('fetch', function(event){
+event.opendWith(
+caches(event.request).then(function(response){
+return response || fetch(event.request)
+})
+)
+
 
 })
